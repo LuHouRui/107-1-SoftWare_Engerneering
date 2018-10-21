2ff6f3a354c5f00d7c41aa029c74c7519b8b50a6
厚銳 呂 <drought0621@Gmail.com>
2018-10-21 15:03:04 +0800

    Create Insert_Database_Exmaple

---
+++ 1018/Insert_DataBase_Example/Insert_DataBase_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Insert_DataBase_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
