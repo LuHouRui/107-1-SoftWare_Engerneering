7abd7d4a388e34441566e1cb245f5ca6cf4341a9
厚銳 呂 <drought0621@Gmail.com>
2018-10-21 15:42:42 +0800

    Complete InsertDataBase Example

--- 1018/Insert_DataBase_Example/Insert_DataBase_Example/Program.cs
+++ 1018/Insert_DataBase_Example/Insert_DataBase_Example/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Insert_DataBase_Example.Models;
 
 namespace Insert_DataBase_Example
 {
@@ -6,7 +9,29 @@ namespace Insert_DataBase_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ImportService.ImportService importService = new ImportService.ImportService();
+            Repository.Repository repository = new Repository.Repository();
+            var nodes = importService.FindOpenData();
+            nodes.ToList()
+                 .ForEach(item =>
+                 {
+                    repository.Insert(item);
+                 });
+            ShowOpenData(nodes);
+            Console.ReadKey();
+        }
+        public static void ShowOpenData(List<OpenData> nodes)
+        {
+            Console.WriteLine(string.Format("共收到{0}筆的資料", nodes.Count));
+            Console.WriteLine("Master");
+            nodes.GroupBy(node => node.資料年度).ToList()
+                .ForEach(group =>
+                {
+                    var key = group.Key;
+                    var groupDatas = group.ToList();
+                    var message = $"資料年度:{key},共有{groupDatas.Count()}筆資料";
+                    Console.WriteLine(message);
+                });
         }
     }
 }
